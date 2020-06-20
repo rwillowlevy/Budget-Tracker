d28444251cb835374481de79ad0f54fd2e405275
Rachel Levy <rwillowlevy@gmail.com>
2020-06-20 14:18:20 -0500

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 
 const app = express();
 
@@ -20,7 +20,6 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
